31ccf385a4dd2ba59516f604193ed9565352a6a5
MorenoEvan <EvanMorenoDeux@gmail.com>
2023-10-03 12:04:04 +0200

    Page de connexion

--- src/main/java/sio/projetjavahelport/HelloController.java
+++ src/main/java/sio/projetjavahelport/HelloController.java
@@ -1,14 +1,41 @@
 package sio.projetjavahelport;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField txtIdentifiant;
+    @FXML
+    private TextField txtMotDePasse;
+    @FXML
+    private Button btnConnexion;
+    @FXML
+    private Label lblMotDePasseOublie;
+    @FXML
+    private Label lblMentionsLegales;
+    @FXML
+    private Label lblContact;
+
+    @FXML
+    public void btnConnexionClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void lblMotDePasseOublieClicked(Event event) {
+    }
+
+    @FXML
+    public void lblMentionLegalesClicked(Event event) {
+    }
+
+    @FXML
+    public void lblContactClicked(Event event) {
     }
 }
