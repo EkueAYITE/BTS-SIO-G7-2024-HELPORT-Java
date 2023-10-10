157e76601fe6559b2fcd443bfcb47b5788d2674c
Etienne <etiennejr.ayite@gmail.com>
2023-10-10 10:57:04 -0700

    interface graphique de contact et des mentions légales

---
+++ src/main/java/sio/projetjavahelport/ContactController.java
@@ -0,0 +1,20 @@
+package sio.projetjavahelport;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ContactController {
+    @javafx.fxml.FXML
+    private TextField txtContactNom;
+    @javafx.fxml.FXML
+    private TextField txtContactPrenom;
+    @javafx.fxml.FXML
+    private TextField txtMail;
+    @javafx.fxml.FXML
+    private Button btnEnvoyer;
+
+    @javafx.fxml.FXML
+    public void btnEnvoyerClicked(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/sio/projetjavahelport/MentionsLegalesController.java
@@ -0,0 +1,12 @@
+package sio.projetjavahelport;
+
+import javafx.scene.text.TextFlow;
+
+public class MentionsLegalesController
+{
+    @javafx.fxml.FXML
+    private TextFlow txtFlMentions;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
