e8a87f71cec3ecdff903049a6e94d2ff2871f5ea
Etienne <etiennejr.ayite@gmail.com>
2023-10-10 12:28:36 -0700

    interface graphique de contact et des mentions légales

--- src/main/java/sio/projetjavahelport/ConnexionController.java
+++ src/main/java/sio/projetjavahelport/ConnexionController.java
@@ -3,11 +3,16 @@ package sio.projetjavahelport;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -36,7 +41,8 @@ public class ConnexionController implements Initializable {
     }
 
     @FXML
-    public void lblMentionLegalesClicked(Event event) {
+    public void lblMentionLegalesClicked(Event event) throws IOException {
+
     }
 
     @FXML
