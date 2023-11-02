134f87a3b7815e14b58f7028d2a9fed9a5bc6293
MorenoEvan <EvanMorenoDeux@gmail.com>
2023-11-02 15:23:58 +0100

    Pop up des différents boutons à faire : régler l'image avec le bon lien

---
+++ src/main/java/sio/projetjavahelport/InscriptionController.java
@@ -0,0 +1,30 @@
+package sio.projetjavahelport;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class InscriptionController {
+    @javafx.fxml.FXML
+    private TextField txtMotDePasseInscription;
+    @javafx.fxml.FXML
+    private Button btnValiderInscription;
+    @javafx.fxml.FXML
+    private Label lblMentionsLegalesInscription;
+    @javafx.fxml.FXML
+    private Label lblContactInscription;
+
+    @javafx.fxml.FXML
+    public void btnConnexionClicked(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void lblMentionLegalesInscriptionClicked(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void lblContactInscriptionClicked(Event event) {
+    }
+}
---
+++ src/main/java/sio/projetjavahelport/ParametresController.java
@@ -0,0 +1,13 @@
+package sio.projetjavahelport;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class ParametresController {
+    @javafx.fxml.FXML
+    private Button btnValiderParametres;
+
+    @javafx.fxml.FXML
+    public void btnValiderParametresClicked(ActionEvent actionEvent) {
+    }
+}
