d73a7c75bc6811bbfd5bb68fe2ec5b60911c9b89
MorenoEvan <EvanMorenoDeux@gmail.com>
2023-12-05 12:21:12 +0100

    Combobox matières remplies

--- src/main/java/sio/projetjavahelport/CreerDemandeController.java
+++ src/main/java/sio/projetjavahelport/CreerDemandeController.java
@@ -1,31 +1,38 @@
 package sio.projetjavahelport;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.AnchorPane;
+import sio.projetjavahelport.tools.ConnexionBDD;
 
-public class CreerDemandeController {
-    @javafx.fxml.FXML
-    private ComboBox cbMatiere;
-    @javafx.fxml.FXML
-    private ComboBox cbSalle;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class CreerDemandeController implements Initializable {
     @javafx.fxml.FXML
     private DatePicker dtDate;
     @javafx.fxml.FXML
     private Button btnValider;
     @javafx.fxml.FXML
     private AnchorPane apnCreerDemande;
-
     @javafx.fxml.FXML
-    public void cbMatiereClicked(ActionEvent actionEvent) {
-        cbMatiere.getItems().addAll();
-    }
-
+    private ComboBox cboMatiere;
     @javafx.fxml.FXML
-    public void cbSalleClicked(ActionEvent actionEvent) {
-    }
+    private ComboBox cboSousMatiere;
+    @javafx.fxml.FXML
+    private TreeView root;
+    @javafx.fxml.FXML
+    private Button btnAjouterSousMatiere;
+    RequeteServiceController requeteServ;
+    ConnexionBDD maCnx;
+
+
 
     @javafx.fxml.FXML
     public void dtDateClicked(ActionEvent actionEvent) {
@@ -33,5 +40,38 @@ public class CreerDemandeController {
 
     @javafx.fxml.FXML
     public void btnValiderClicked(ActionEvent actionEvent) {
+
+    }
+
+    @javafx.fxml.FXML
+    public void btnAjouterSousMatiereClicked(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cboMatiereClicked(ActionEvent actionEvent) {
+
+    }
+
+    @javafx.fxml.FXML
+    public void cboSousMatiereClicked(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try{
+            maCnx = new ConnexionBDD();
+            requeteServ = new RequeteServiceController();
+
+            ArrayList<String> matieres = requeteServ.GetAllMatieres();
+            for(String matiere : matieres){
+                cboMatiere.getItems().add(matiere);
+            }
+
+        }catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        cboMatiere.getSelectionModel().selectFirst();
     }
 }
